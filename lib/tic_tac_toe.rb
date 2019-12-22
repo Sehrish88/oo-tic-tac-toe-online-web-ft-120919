3641b4e8ad372809d99e3f8359bbb8f26ec8bc9e
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-22 15:30:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,10 @@ class TicTacToe
     ((@board[index_i] == "X") || (@board[index_i] == "O"))
   end
   
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
   
 
 
